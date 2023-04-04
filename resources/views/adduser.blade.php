eae931352ce27734e2aa5fee57a075630cd396e7
Jeelpatel0903 <71834522+Jeelpatel0903@users.noreply.github.com>
2023-04-04 15:23:15 +0530

    upload
    upload

---
+++ resources/views/adduser.blade.php
@@ -0,0 +1,108 @@
+
+@extends('layout.app')
+@section('content')
+
+<style>
+#addbtn{
+    margin-left: 90%;
+}
+</style>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>DataTables</h1>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">DataTable with default features</h3>
+                <button type="button" class="btn btn-primary" id="addbtn">Add</button>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>Rendering engine</th>
+                    <th>Browser</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                    <th colspan="2">Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Misc</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>C</td>
+                    <td><button type="button" class="btn btn-success">Update</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                  </tr>
+                  <tr>
+                    <td>Misc</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>C</td>
+                    <td><button type="button" class="btn btn-success">Update</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                  </tr>
+                  <tr>
+                    <td>Misc</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>C</td>
+                    <td><button type="button" class="btn btn-success">Update</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                  </tr>
+                  <tr>
+                    <td>Misc</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>C</td>
+                    <td><button type="button" class="btn btn-success">Update</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                  </tr>
+                  <tr>
+                    <td>Misc</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>C</td>
+                    <td><button type="button" class="btn btn-success">Update</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+    @endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Google Font: Source Sans Pro -->
+<!-- extra -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+<!-- extra -->
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+</head>
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+
+
+@include('layout.header')
+
+    @include('layout.sidebar')
+
+    @yield('content')
+
+    @include('layout.footer')
+
+
+</div>
+</body>
+</html>
